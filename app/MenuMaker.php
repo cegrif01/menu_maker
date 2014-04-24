3f202b69b6d663031385a1ebc65db80dcd9b2801
Charles Griffin <cgriffin@indatus.com>
2014-04-23 20:48:25 -0400

    fixed the csv code tightly coupled (on purpose) in the MenuMaker

--- app/MenuMaker.php
+++ app/MenuMaker.php
@@ -5,45 +5,40 @@ class MenuMaker
 	public function makeMenu()
 	{
 		try {
-				foreach($_POST['menu'] as $value) {
-			
-					foreach($value as $key => $field) {
+            
+			foreach($_POST['menu'] as $value) {
+		
+				foreach($value as $key => $field) {
 
-						if(empty($field) || is_null($field)) {
+					if(empty($field) || is_null($field)) {
 
-							throw new Exception("$key was empty");
-						}
-
-						if($key == "price" && !is_numeric($field)) {
-							
-							throw new Exception("$field is not an number");
-						}
+						throw new Exception("$key was empty");
 					}
-			
+
+					if($key == "price" && !is_numeric($field)) {
+						
+						throw new Exception("$field is not an number");
+					}
 				}
-
-				$csv_file = 'file.csv';
-				header( "Content-Type: text/csv;charset=utf-8" );
-				header( "Content-Disposition: attachment;filename=\"$csv_file\"" );
-				header("Pragma: no-cache");
-				header("Expires: 0");
-
-				$fp = fopen('php://output', 'w');
-
-				array_unshift($_POST['menu'], array_keys($_POST['menu'][0]));
-				foreach($_POST['menu'] as $menu_item) {
-
-					fputcsv($fp, $menu_item);
-				}
-
-				fclose($fp);
-				die();
-					
-			} catch(Exception $e) {
-
-				echo $e->getMessage();
-
 			}
+
+		    //will download csv file to the location specified by the string
+            $fp = fopen('/home/charles/Downloads/file.csv', 'w');
+            
+            array_unshift($_POST['menu'], array_keys($_POST['menu'][0]));
+            foreach($_POST['menu'] as $menu_item) {
+
+                fputcsv($fp, $menu_item);
+                
+            }
+
+            fclose($fp);
+					
+		} catch(Exception $e) {
+
+			echo $e->getMessage();
+
+		}
 		
 	}// end of makeMenu
 }

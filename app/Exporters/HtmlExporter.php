b3a3338d64f3a831fc039e99755dd79f39b4483a
Charles Griffin <cgriffin@indatus.com>
2014-05-25 20:56:08 -0400

    added my menu in html format

--- app/Exporters/HtmlExporter.php
+++ app/Exporters/HtmlExporter.php
@@ -19,25 +19,37 @@ class HtmlExporter implements Exportable
         $headers .= "Content-type: text/html\r\n";
 
         //begin of HTML message 
-        $message = <<<EOF
-        <html> 
-          <body bgcolor="#DCEEFC"> 
-            <center> 
-                <b>This is a test</b> <br> 
-                <font color="red">Thanks Charles Griffin!</font> <br> 
-                <a href="http://www.billionfactorial.com/">check out my blog</a> 
-            </center> 
-              <br><br>*** Now you Can send HTML Email <br> Regards<br>Charles Griffin
-          </body> 
-        </html> 
-EOF;
+        $message = '<html><body>';
+
+        $message .= '<h1>Your Custom Menu</h1><table>';
+
+        $message .= '<tr>';
+
+        foreach($_POST['menu'] as $key => $menu_items) {
+
+            $message .= '<tr>';
+            if($key == 0) {
+
+                foreach(array_keys($menu_items) as $item) {
+                    $message .= "<th>$item</th>";
+                }
+            }
+
+            $message .= '</tr>';
+
+
+            $message .= '<tr>';
+
+            foreach(array_values($menu_items) as $item) {
+                $message .= "<td>$item</td>";
+            }
+            
+            $message .= '</table></body></html>';
+        }
 
         if(mail('cegrif01@gmail.com', 'html email', $message, $headers)) {
-            pp('mail sent');
-            return true;
+            echo 'mail sent';
         }
-        pp('fail');
-        return false;
     }
 
 }

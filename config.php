926e91524d897f0786edd12e1597491d00272aa0
Charles Griffin <cgriffin@indatus.com>
2014-06-06 14:15:57 -0400

    almost finished with dependency inversion.  Added a config file

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'email_address' => 'cegrif01@gmail.com',
+    'email' => 'cegrif01@gmail.com',
     'csv_file'      => getcwd().'/file.csv',
     'html_file'     => getcwd().'/menu.html',
     'email_subject' => "Hello! This is HTML email"
